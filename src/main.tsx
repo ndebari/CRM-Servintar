29279a6760abedfdfacacede85bdfb6b3c16b3e6
ndebari <ndebari@servintar.com.ar>
2026-09-14 17:15:52 -0300

    Clear legacy PWA caches on startup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
+async function clearLegacyPwaCache() {
+  if ('serviceWorker' in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    await Promise.all(registrations.map((registration) => registration.unregister()));
+  }
+
+  if ('caches' in window) {
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+  }
+}
+
+void clearLegacyPwaCache();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
